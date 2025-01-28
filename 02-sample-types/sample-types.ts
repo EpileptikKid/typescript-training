51dfc1cb8943a71ca9a45827efc27758508ec1a2
Andrii Voloskovets <k1d.ep1lept1k@gmail.com>
2025-01-29 01:50:50 +0200

    create simple variables

---
+++ 02-sample-types/sample-types.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName = "Andrii";
+let lastName = "Voloskovets";
+
+console.log(found);
+console.log("The grade is " + grade);
+
+console.log("Hi " + firstName + " " + lastName);
+console.log(`Hi ${firstName} ${lastName}`);
